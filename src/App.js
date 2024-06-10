5ce3faf5b96427cbcbee4ba3a6552c344e5b31a8
Niraj Kumar <104626456+niraj142316@users.noreply.github.com>
2024-06-10 21:13:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Reviews from './components/reviews/Reviews';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import Sliders from './components/sliderContent/Sliders';
+import {data, responsive} from './reviewData';
 import Home from './components/home/Home';
 import Blog from './components/blog/Blog';
 import Featured from './components/featured/Featured';
@@ -11,12 +16,24 @@ import Faqs from './components/faqs/Faqs';
 import Footer from './components/footer/Footer';
 
 function App() {
+
+    const slider=data.map(item=>(
+      <Sliders name={item.name} star={item.star} desc={item.desc} profile={item.profile} iconTick={item.iconTick} title={item.title} icon={item.icon} tagline={item.tagline} subtag={item.subtag} />
+    ))
+
+
   return (
     <div className="App">
       <Navbar />
       <Home />
       <Blog />
       <Featured />
+      <div className="carosel">
+      <Reviews />
+      <Carousel showDots={true} infinite={true} responsive={responsive}>
+        {slider}
+      </Carousel>
+      </div>
       <Services />
       <Build />
       <Graphic />
