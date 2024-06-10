6cc970d0eaa84c93d90f73962d00b0b46bd78e4b
Niraj Kumar <104626456+niraj142316@users.noreply.github.com>
2024-06-10 21:12:02 +0530

    Add files via upload

---
+++ src/reviewData.js
@@ -0,0 +1,93 @@
+export const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 768 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 768, min: 0 },
+      items: 1
+    }
+  }
+
+export const data = [
+    {
+      id: 1,
+      star:"https://img.icons8.com/?size=48&id=qdQpy48X3Rjv&format=png",
+      desc:"Lorem ipsum dolor sit amet consectetur adipis hello welcome to the demo website and me intro you to this webs he he helo elit. Placeat magnam dolorem.",
+      profile:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtpwe4gV2OYx9BiAF9gOcFNsP9MlSaWMGdu7LDDFurkFACMu3Fi_BrirlRTpojJT3oaHU&usqp=CAU",
+      name: "Sarah Taylor",
+      iconTick:"https://t3.ftcdn.net/jpg/05/43/29/02/360_F_543290296_snhXYYelZwXmXoo1sUoVMD54GXTPguWH.jpg",
+      title: "Customer",
+      icon: "https://png.pngtree.com/png-clipart/20200720/original/pngtree-river-logo-template-vector-icon-illustration-design-png-image_4781539.jpg",
+      tagline:"C  O  M  P  A  N  Y",
+      subtag:"TAGLINE GOES HERE"
+    },
+    {
+      id: 2,
+      star:"https://img.icons8.com/?size=48&id=qdQpy48X3Rjv&format=png",
+      desc:"Lorem ipsum dolor sit amet consectetur adipis hello welcome to the demo website and me intro you to this webs he he helo elit. Placeat magnam dolorem.",
+      profile:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtpwe4gV2OYx9BiAF9gOcFNsP9MlSaWMGdu7LDDFurkFACMu3Fi_BrirlRTpojJT3oaHU&usqp=CAU",
+      name: "Emma Watson",
+      iconTick:"https://t3.ftcdn.net/jpg/05/43/29/02/360_F_543290296_snhXYYelZwXmXoo1sUoVMD54GXTPguWH.jpg",
+      title: "Customer",
+      icon: "https://png.pngtree.com/png-clipart/20200720/original/pngtree-river-logo-template-vector-icon-illustration-design-png-image_4781539.jpg",
+      tagline:"C  O  M  P  A  N  Y",
+      subtag:"TAGLINE GOES HERE"
+    },
+    {
+      id: 3,
+      star:"https://img.icons8.com/?size=48&id=qdQpy48X3Rjv&format=png",
+      desc:"Lorem ipsum dolor sit amet consectetur adipis hello welcome to the demo website and me intro you to this webs he he helo elit. Placeat magnam dolorem.",
+      profile:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtpwe4gV2OYx9BiAF9gOcFNsP9MlSaWMGdu7LDDFurkFACMu3Fi_BrirlRTpojJT3oaHU&usqp=CAU",
+      name: "Erica M.",
+      iconTick:"https://t3.ftcdn.net/jpg/05/43/29/02/360_F_543290296_snhXYYelZwXmXoo1sUoVMD54GXTPguWH.jpg",
+      title: "Customer",
+      icon: "https://png.pngtree.com/png-clipart/20200720/original/pngtree-river-logo-template-vector-icon-illustration-design-png-image_4781539.jpg",
+      tagline:"C  O  M  P  A  N  Y",
+      subtag:"TAGLINE GOES HERE"
+    },
+    {
+      id: 4,
+      star:"https://img.icons8.com/?size=48&id=qdQpy48X3Rjv&format=png",
+      desc:"Lorem ipsum dolor sit amet consectetur adipis hello welcome to the demo website and me intro you to this webs he he helo elit. Placeat magnam dolorem.",
+      profile:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtpwe4gV2OYx9BiAF9gOcFNsP9MlSaWMGdu7LDDFurkFACMu3Fi_BrirlRTpojJT3oaHU&usqp=CAU",
+      name: "Erica M.",
+      iconTick:"https://t3.ftcdn.net/jpg/05/43/29/02/360_F_543290296_snhXYYelZwXmXoo1sUoVMD54GXTPguWH.jpg",
+      title: "Customer",
+      icon: "https://png.pngtree.com/png-clipart/20200720/original/pngtree-river-logo-template-vector-icon-illustration-design-png-image_4781539.jpg",
+      tagline:"C  O  M  P  A  N  Y",
+      subtag:"TAGLINE GOES HERE"
+    },
+    {
+      id: 5,
+      star:"https://img.icons8.com/?size=48&id=qdQpy48X3Rjv&format=png",
+      desc:"Lorem ipsum dolor sit amet consectetur adipis hello welcome to the demo website and me intro you to this webs he he helo elit. Placeat magnam dolorem.",
+      profile:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtpwe4gV2OYx9BiAF9gOcFNsP9MlSaWMGdu7LDDFurkFACMu3Fi_BrirlRTpojJT3oaHU&usqp=CAU",
+      name: "Erica M.",
+      iconTick:"https://t3.ftcdn.net/jpg/05/43/29/02/360_F_543290296_snhXYYelZwXmXoo1sUoVMD54GXTPguWH.jpg",
+      title: "Customer",
+      icon: "https://png.pngtree.com/png-clipart/20200720/original/pngtree-river-logo-template-vector-icon-illustration-design-png-image_4781539.jpg",
+      tagline:"C  O  M  P  A  N  Y",
+      subtag:"TAGLINE GOES HERE"
+    },
+    {
+      id: 6,
+      star:"https://img.icons8.com/?size=48&id=qdQpy48X3Rjv&format=png",
+      desc:"Lorem ipsum dolor sit amet consectetur adipis hello welcome to the demo website and me intro you to this webs he he helo elit. Placeat magnam dolorem.",
+      profile:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtpwe4gV2OYx9BiAF9gOcFNsP9MlSaWMGdu7LDDFurkFACMu3Fi_BrirlRTpojJT3oaHU&usqp=CAU",
+      name: "Erica M.",
+      iconTick:"https://t3.ftcdn.net/jpg/05/43/29/02/360_F_543290296_snhXYYelZwXmXoo1sUoVMD54GXTPguWH.jpg",
+      title: "Customer",
+      icon: "https://png.pngtree.com/png-clipart/20200720/original/pngtree-river-logo-template-vector-icon-illustration-design-png-image_4781539.jpg",
+      tagline:"C  O  M  P  A  N  Y",
+      subtag:"TAGLINE GOES HERE"
+    },
+  ]
